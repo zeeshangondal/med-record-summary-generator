cc730d884f29bf71eb5b9091a7ba1e5a89304c17
zeeshangondal <zeeshangondal0007@gmail.com>
2024-08-28 22:24:19 +0500

    setup, llama prep, embeddings generator, query data implemented

---
+++ get_embedding_function.py
@@ -0,0 +1,9 @@
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+
+
+def get_embedding_function():
+    # embeddings = BedrockEmbeddings(
+    #     credentials_profile_name="default", region_name="us-east-1"
+    # )
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    return embeddings
---
+++ query_data.py
@@ -0,0 +1,56 @@
+import argparse
+from langchain.vectorstores.chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+
+from get_embedding_function import get_embedding_function
+
+import warnings
+warnings.filterwarnings("ignore")
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag(query_text)
+
+
+def query_rag(query_text: str):
+    # Prepare the DB.
+    embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_score(query_text, k=5)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    # print(prompt)
+
+    model = Ollama(model="gemma2:2b")
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(response_text)
+    return response_text
+
+
+if __name__ == "__main__":
+    main()
